15103f3fd9797e86b6213c0dffc9d11470917cef
Mark Luoma <luomamr@gmail.com>
2018-12-12 15:59:32 -0500

    final touches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,27 +8,22 @@ var config = {
     messagingSenderId: "641341274833"
 };
 firebase.initializeApp(config);
-
-
 var database = firebase.database();
-
+///on click function and assigning the inputs///
 $("#button").on("click", function(){
     event.preventDefault();
-
     var name =$("#trainName_input").val().trim();
     var destination = $("#destination_input").val().trim();
     var time = moment($("#trainTime_input").val().trim(), "HH:mm").format("X");
     var frequency = $("#frequency_input").val().trim();
-
-    
-    
+    ///variables for new train,place, time, freq////
     var nextTrain = {
         newName: name,
         newDestination: destination,
         newTime: time,
         newFrequency: frequency
     };
-
+///push inputs to firebase database///
     database.ref().push(nextTrain);
 
     console.log(nextTrain.name);
@@ -37,14 +32,12 @@ $("#button").on("click", function(){
     console.log(nextTrain.frequency);
 
     alert("Successfully Added");
-
+///empty inputs after submitting//
     $("#trainName_input").val("");
     $("#destination_input").val("");
     $("#trainTime_input").val("");
     $("#frequency_input").val("");
-
 });
-
 database.ref().on("child_added", function(childSnapshot){
     console.log(childSnapshot.val());
 
@@ -60,29 +53,22 @@ database.ref().on("child_added", function(childSnapshot){
 
     var convert = moment(time, "hh:mm").subtract(1, "years");
     console.log(convert);
-
     ////// current Time
     var currentTime = moment();
     console.log("current time" + moment(currentTime).format("hh:mm"));
-
     ////// difference between the times
     var diffTime = moment().diff(moment(convert), "minutes");
     console.log("difference:" + diffTime);
-
     // ////remainder
     var Remainder = diffTime % frequency;
     console.log(Remainder);
-
     /////minutes Until next Train
     var MinutesTillTrain = frequency - Remainder;
     console.log("Min until train:" + MinutesTillTrain);
-
     ///// next Train
     var upcomingTrain = moment().add(MinutesTillTrain, "minutes");
     console.log("arrival: " + moment(upcomingTrain).format("hh:mm"));
-
-   
-
+///append the table with new info
     var newRow = $("<tr>").append(
         $("<td>").text(name),
         $("<td>").text(destination),
